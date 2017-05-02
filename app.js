9e94ecfb8b9ab51f29d811cc741c77787fc123d5
Andrew Haupt <ajhaupt7@gmail.com>
2017-05-02 16:00:10 -0700

    Can take a path to place donwloaded files

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const fs        = require('fs');
-const axios     = require('axios');
-const url       = require('url');
-const parseArgs = require('minimist');
-const random    = require('lodash/random');
+const fs         = require('fs');
+const axios      = require('axios');
+const url        = require('url');
+const parseArgs  = require('minimist');
+const random     = require('lodash/random');
+const mkdirp     = require('mkdirp');
+const os         = require('os');
+const getDirName = require('path').dirname;
 
 const createNumRange = (num) => {
   const numString = num ? num.toString() : '[800,1600]';
@@ -11,12 +14,32 @@ const createNumRange = (num) => {
     num.split(",");
 }
 
+const formatPath = (path) => {
+  let formattedPath = path.replace("~", os.homedir());
+  if (fs.existsSync(formattedPath)) {
+    return formattedPath
+  } else {
+    console.log(`${path} does not exist, creating directory`)
+    mkdirp(formattedPath, err => {
+      console.log(err)
+    });
+    return formattedPath
+  }
+} 
+
 const args      = parseArgs(process.argv.slice(2)),
+      help      = args.help,
       number    = args.number <= 50 ? (args.number <= 50 ? args.number : 50) : null,
       keywords  = args.keywords,
-      name      = `${keywords ? keywords.replace(",", "_") : ''}_photo`,
+      prefix    = args.prefix || `${keywords ? keywords.replace(",", "_") : ''}_photo`,
       width     = createNumRange(args.width),
-      height    = createNumRange(args.height);
+      height    = createNumRange(args.height),
+      writeDir  = args.path ? formatPath(args.path) : '.';
+
+if (help) {
+  console.log("Options:");
+  return false; 
+}
 
 if (!number) {
   console.log("You have to specify the number of images to download.");
@@ -26,20 +49,20 @@ if (!number) {
 const fetchImage = (name, count) => {
   const searchUrl = `https://source.unsplash.com/${random(width[0],width[1])}x${random(height[0],height[1])}/?${keywords || ''}`;
 
-  console.log(searchUrl)
-
   return axios({
     method:'get',
     url: searchUrl,
     responseType:'stream'
   })
   .then((response) => {
-    console.log(`Downloading Photo ${count}`)
     const { responseUrl } = response.data,
           urlParts = url.parse(responseUrl, true),
-          fileExt = urlParts.query.fm;
+          fileExt = urlParts.query.fm,
+          fullPath = `${writeDir}/${prefix}_${count}.${fileExt}`;
+
+    console.log(`Downloading photo to ${fullPath}`)
    
-    return response.data.pipe(fs.createWriteStream(`${name}_${count}.${fileExt}`))
+    return response.data.pipe(fs.createWriteStream(fullPath))
   })
   .catch((error) => {
     console.log(error);
@@ -47,7 +70,7 @@ const fetchImage = (name, count) => {
 }
 
 const promisedImages = [...Array(Number(number))].map((_,i) => {
-  return fetchImage(name, i);
+  return fetchImage(prefix, i);
 });
 
 return Promise.all(promisedImages).then(() => console.log('Finished Downloading!'))
